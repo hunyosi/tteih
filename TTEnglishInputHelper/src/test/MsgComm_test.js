df367b64f27b122d923ddb3e6bfd3ecd421e1389
Hunyosi Asakura <hunyosi@hunyoi.com>
2016-04-15 15:02:10 +0900

    add test 'onReceiveMessage invoke'

--- TTEnglishInputHelper/src/test/MsgComm_test.js
+++ TTEnglishInputHelper/src/test/MsgComm_test.js
@@ -264,22 +264,120 @@ describe('MsgComm', function () {
       };
       mcs.onReceiveMessage({}, '{"id":1, "msg":"new", "data":{"cls":"Abc"}}');
 
-      // const objId2 = mcs.dispatchMessage('new', {cls:'Abc'});
-      // actual = objId2;
-      // assert.strictEqual(/*expected*/2, actual);
-      //
-      // actual = mcs.dispatchMessage('invoke', {cls:'Abc', seqNo:objId, method:'add', params:[3]});
-      // assert.strictEqual(/*expected*/void 0, actual);
-      // actual = mcs.dispatchMessage('invoke', {cls:'Abc', seqNo:objId, method:'getCount', params:[]});
-      // assert.strictEqual(/*expected*/3, actual);
-      // actual = mcs.dispatchMessage('invoke', {cls:'Abc', seqNo:objId2, method:'add', params:[1]});
-      // assert.strictEqual(/*expected*/void 0, actual);
-      // actual = mcs.dispatchMessage('invoke', {cls:'Abc', seqNo:objId2, method:'getCount', params:[]});
-      // assert.strictEqual(/*expected*/1, actual);
-      // actual = mcs.dispatchMessage('invoke', {cls:'Abc', seqNo:objId, method:'add', params:[2]});
-      // assert.strictEqual(/*expected*/void 0, actual);
-      // actual = mcs.dispatchMessage('invoke', {cls:'Abc', seqNo:objId, method:'getCount', params:[]});
-      // assert.strictEqual(/*expected*/5, actual);
+    });
+    it('onReceiveMessage invoke', function(){
+      class ClsA {
+        constructor(){
+          this.count = 0;
+        }
+        add(val){
+          this.count += val;
+        }
+        getCount(){
+         return this.count;
+        }
+      }
+      const cs = new MsgComm.CommunicatorStub();
+      const mcs = new MsgComm.MsgCommServer(cs);
+      mcs.addClassWithName('Abc', ClsA);
+
+      let objId;
+
+      cs.onResponse = (msgStr) => {
+        objId = JSON.parse(msgStr).data;
+      };
+      mcs.onReceiveMessage({}, '{"id":1, "msg":"new", "data":{"cls":"Abc"}}');
+
+      cs.onResponse = (actual) => {
+        assert.deepEqual(/*expected*/{
+          id:2,
+          msg:'invoke',
+          ok:true,
+          data:0
+        }, JSON.parse(actual));
+      };
+      mcs.onReceiveMessage({}, `{"id":2, "msg":"invoke", "data":{"cls":"Abc", "seqNo":${objId}, "method":"getCount", "params":[]}}`);
+
+      cs.onResponse = (actual) => {
+        assert.deepEqual(/*expected*/{
+          id:3,
+          msg:'invoke',
+          ok:true,
+          data:[]
+        }, JSON.parse(actual));
+      };
+      mcs.onReceiveMessage({}, `{"id":3, "msg":"invoke", "data":{"cls":"Abc", "seqNo":${objId}, "method":"add", "params":[[7]]}}`);
+
+      cs.onResponse = (actual) => {
+        assert.deepEqual(/*expected*/{
+          id:4,
+          msg:'invoke',
+          ok:true,
+          data:7
+        }, JSON.parse(actual));
+      };
+      mcs.onReceiveMessage({}, `{"id":4, "msg":"invoke", "data":{"cls":"Abc", "seqNo":${objId}, "method":"getCount", "params":[]}}`);
+    });
+    it('onReceiveMessage invoke promise', function(){
+      class ClsA {
+        constructor(){
+          this.count = 0;
+        }
+        add(val){
+          this.count += val;
+        }
+        getCount(){
+          return new Promise((resolve, reject)=>{
+            setTimeout(()=>resolve(this.count), 0);
+          });
+        }
+      }
+      const cs = new MsgComm.CommunicatorStub();
+      const mcs = new MsgComm.MsgCommServer(cs);
+      mcs.addClassWithName('Abc', ClsA);
+
+      return new Promise((resolve)=>{
+        let objId;
+
+        cs.onResponse = (msgStr) => {
+          objId = JSON.parse(msgStr).data;
+        };
+        mcs.onReceiveMessage({}, '{"id":1, "msg":"new", "data":{"cls":"Abc"}}');
+
+        cs.onResponse = (actual) => {
+          assert.deepEqual(/*expected*/{
+            id:2,
+            msg:'invoke',
+            ok:true,
+            data:0
+          }, JSON.parse(actual));
+
+          cs.onResponse = (actual) => {
+            assert.deepEqual(/*expected*/{
+              id:3,
+              msg:'invoke',
+              ok:true,
+              data:[]
+            }, JSON.parse(actual));
+
+            cs.onResponse = (actual) => {
+              assert.deepEqual(/*expected*/{
+                id:4,
+                msg:'invoke',
+                ok:true,
+                data:7
+              }, JSON.parse(actual));
+
+              resolve();
+            };
+            mcs.onReceiveMessage({}, `{"id":4, "msg":"invoke", "data":{"cls":"Abc", "seqNo":${objId}, "method":"getCount", "params":[]}}`);
+
+          };
+          mcs.onReceiveMessage({}, `{"id":3, "msg":"invoke", "data":{"cls":"Abc", "seqNo":${objId}, "method":"add", "params":[[7]]}}`);
+
+        };
+        mcs.onReceiveMessage({}, `{"id":2, "msg":"invoke", "data":{"cls":"Abc", "seqNo":${objId}, "method":"getCount", "params":[]}}`);
+      });
     });
   });
 });
