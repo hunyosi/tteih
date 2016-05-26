3ff5f37e1a8c93c7a4c1d5a73ac8e33cc59f2e87
Hunyosi Asakura <hunyosi@hunyoi.com>
2016-05-26 13:49:42 +0900

    corrected path_test.js

--- TTEnglishInputHelper/src/test/path_test.js
+++ TTEnglishInputHelper/src/test/path_test.js
@@ -8,16 +8,16 @@ describe('path', function() {
   describe('RouteElement', function() {
     describe('constructor', function() {
       it('default', function() {
-        const actual = new path.RouteElement();
-        assert.equal( /*expected*/true, true);
+        const actual = new path.RouteElement({});
+        assert.strictEqual( /*expected*/path.RouteElement, actual.constructor);
       });
     });
   });
   describe('Path', function() {
     describe('constructor', function() {
       it('default', function() {
-        const actual = new path.Path();
-        assert.equal( /*expected*/true, true);
+        const actual = new path.Path({});
+        assert.strictEqual( /*expected*/path.Path, actual.constructor);
       });
     });
   });
