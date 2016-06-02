fdbec0584bf0269dfd8fb111f70d3a9d97b4511a
Hunyosi Asakura <hunyosi@hunyoi.com>
2016-06-02 09:50:17 +0900

    add parseWindowsPath(), buildWindowsPath()

--- TTEnglishInputHelper/src/main/common/path.js
+++ TTEnglishInputHelper/src/main/common/path.js
@@ -2,9 +2,13 @@
 
 export class RouteElement {
   constructor({name, isCurrent, isParent}) {
-    this.name = name;
+    this.name = name || '';
     this.isCurrent = !!isCurrent;
     this.isParent = !!isParent;
+  }
+
+  get isNormal() {
+    return (!this.isCurrent) && (!this.isParent) && (typeof this.name === 'string');
   }
 }
 
@@ -84,7 +88,6 @@ export function buildUnixPath(pathObj) {
   }
 
   let sep = '';
-  console.log(pathObj.route);
   for (let elm of pathObj.route) {
     buf += sep;
     if (elm.isParent) {
@@ -95,6 +98,88 @@ export function buildUnixPath(pathObj) {
       buf += elm.name;
     }
     sep = '/';
+  }
+
+  return buf;
+}
+
+
+export function parseWindowsPath(pathStr) {
+  let host = null;
+  let drive = null;
+  let pathBody = pathStr;
+  const matchHost = pathStr.match(/^[\\\/][\\\/]([^.?\\\/]+)(.*)$/);
+  if (matchHost !== null) {
+    host = matchDriveLetter[1];
+    pathBody = matchDriveLetter[2];
+  } else {
+    const matchDriveLetter = pathStr.match(/^([A-Za-z]):(.*)$/);
+    if (matchDriveLetter !== null) {
+      drive = matchDriveLetter[1];
+      pathBody = matchDriveLetter[2];
+    }
+  }
+
+  const topChar = pathStr.charAt(0);
+  const isAbsolute = (topChar === '\\' || topChar === '/');
+  if (isAbsolute) {
+    pathBody = pathStr.substring(1);
+  }
+  const route = [];
+  for (let elm of pathBody.split(/[\\\/]/)) {
+    if (elm.length < 1) {
+    } else if (elm === '.') {
+      route.push(new RouteElement({isCurrent:true}));
+    } else if (elm === '..') {
+      route.push(new RouteElement({isParent:true}));
+    } else {
+      route.push(new RouteElement({name:elm}));
+    }
+  }
+
+  let adsName = null;
+  if (0 < route.length && route[route.length - 1].isNormal) {
+    const parts = route[route.length - 1].split(/:/);
+    if (parts.length > 1) {
+      route[route.length - 1] = new RouteElement({name:parts[0]});
+      adsName = parts[parts.length - 1];
+    }
+  }
+
+  return new Path({route, isAbsolute, drive, host, adsName});
+}
+
+
+export function buildWindowsPath(pathObj) {
+  let buf = '';
+
+  if (pathObj.host != null) {
+    buf += '\\' + pathObj.host;
+  }
+
+  if (pathObj.drive != null) {
+    buf += pathObj.drive + ':';
+  }
+
+  if (pathObj.isAbsolute) {
+    buf += '\\';
+  }
+
+  let sep = '';
+  for (let elm of pathObj.route) {
+    buf += sep;
+    if (elm.isParent) {
+      buf += '..';
+    } else if (elm.isCurrent) {
+      buf += '.';
+    } else {
+      buf += elm.name;
+    }
+    sep = '\\';
+  }
+
+  if (pathObj.adsName != null) {
+    buf += ':' + pathObj.adsName;
   }
 
   return buf;
