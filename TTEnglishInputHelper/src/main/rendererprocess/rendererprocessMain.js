c44c1e6f8bdfa5cf35021222fbe7408e43318883
Hunyosi Asakura <hunyosi@hunyoi.com>
2016-06-21 13:30:26 +0900

    corrected rendererprocessMain.js

--- TTEnglishInputHelper/src/main/rendererprocess/rendererprocessMain.js
+++ TTEnglishInputHelper/src/main/rendererprocess/rendererprocessMain.js
@@ -1,15 +1,15 @@
 'use strict';
 
 import * as renderercomm from '../electron/renderercomm.js';
-import * as msgcommm from '../common/MsgComm.js';
+import * as msgcomm from '../common/MsgComm.js';
 import * as pathutils from '../common/path.js';
 
-const commClient = renderercomm.RendererCommunicatorClient('msgcomm');
-const rdrMsgComm = msgcomm.MsgCommClient(commClient, [pathutils.Path]);
+const commClient = new renderercomm.RendererCommunicatorClient('msgcomm');
+const rdrMsgComm = new msgcomm.MsgCommClient(commClient, [pathutils.Path]);
 
 document.addEventListener('DOMContentLoaded', ()=>{
   document.body.appendChild(document.createElement('p').appendChild(document.createTextNode('hello, world')));
-  rdrMsgComm.fetch().then(()=>{
+  rdrMsgComm.fetchClass().then(()=>{
     document.body.appendChild(document.createElement('p').appendChild(document.createTextNode('hello, world2')));
   });
 });
